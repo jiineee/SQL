8d740a201814c6bbc8d42c50eec157bd735dd306
jiineee <jangjiin0917@gmail.com>
2023-05-31 13:56:39 +0900

    SQL

---
+++ 13트랙잭션.sql
@@ -0,0 +1,54 @@
+--트랜잭션 (논리적 작업단위)
+
+SHOW AUTOCOMMIT;
+--오토 커밋 온
+SET AUTOCOMMIT ON;
+--오토 커밋 오프
+SET AUTOCOMMIT OFF;
+
+DELETE FROM DEPTS WHERE DEPARTMENT_ID = 10;
+SAVEPOINT DELETE10; --세이브포인트 기록
+
+DELETE FROM DEPTS WHERE DEPARTMENT_ID = 20;
+SAVEPOINT DELETE20; --세이브포인트 기록 
+
+SELECT * FROM DEPTS;
+
+ROLLBACK TO DELETE10; --10번 세이브포인트로 롤백
+SELECT * FROM DEPTS;
+
+ROLLBACK; --마지막 커밋 시점
+SELECT * FROM DEPTS;
+
+------------------------------------------------
+INSERT INTO DEPTS VALUES(300,'DEMO',NULL ,1800);
+
+COMMIT; -- 트랜잭션 반영
+
+SELECT * FROM DEPTS;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
